a1edd04c0bb42826ff8d5553e1c30e3902123811
Xorya <130937260+xorya1@users.noreply.github.com>
2024-07-18 22:51:39 +0100

    Delete ScriptChecks.ps1

--- ScriptChecks.ps1
+++
@@ -1,8 +0,0 @@
-$availableUpdates = Get-WindowsUpdate 
-
-if ($availableUpdates) {
-    Write-Host "Available updates:"
-    $availableUpdates | Format-Table -AutoSize
-} else {
-    Write-Host "No updates are currently available."
-}
