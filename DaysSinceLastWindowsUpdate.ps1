5cc0e1362661d34c06f4440cf95a6dbcd947cc1d
Xorya <130937260+xorya1@users.noreply.github.com>
2024-07-18 22:51:09 +0100

    Delete DaysSinceLastWindowsUpdate.ps1

--- DaysSinceLastWindowsUpdate.ps1
+++
@@ -1,8 +0,0 @@
-#Count Days Since last Windows Update Was Run
-#Author: Sean Bradley
-#License: BSD-3-Clause-Attribution
-#Attribution: https://sbcode.net/zabbix/powershell-windows-updates/
-$date = Get-Date
-$diff = (Get-HotFix | Sort-Object -Property InstalledOn)[-1] | Select-Object InstalledOn
-$diff3 = New-TimeSpan -Start $diff.InstalledOn -end $date
-write-host $diff3.days
